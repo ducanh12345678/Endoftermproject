005739b94be423a22b800ad87564ab2d9093bb18
ducanh12345678 <108135655+ducanh12345678@users.noreply.github.com>
2022-09-30 18:44:33 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var MainImg = document.getElementById("MainImg")
+var Smallimg = document.getElementsByClassName("small-img")
+Smallimg[0].onclick = function(){
+    MainImg.src = Smallimg[0].src;
+}
+Smallimg[1].onclick = function(){
+    MainImg.src = Smallimg[1].src;
+}
+Smallimg[2].onclick = function(){
+    MainImg.src = Smallimg[2].src;
+}
+Smallimg[3].onclick = function(){
+    MainImg.src = Smallimg[3].src;
+}
